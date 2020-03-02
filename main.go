6a683395deb84d23ad7ad895e103da5e38777991
Muhamad Risman <real.risman@gmail.com>
2020-03-02 22:34:22 +0700

    Add basic logic for CreateConsignment

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	// import the generated protobuf code
+	"sync"
+
+	pb "github.com/realrisman/go-micro-grpc/proto/consignment"
+)
+
+const port = ":50051"
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+}
+
+// Repository - Dummy repository, this simulates the use of a datastore of some kind.
+type Repository struct {
+	mu           sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+// Create a new consignment
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.mu.Unlock()
+
+	return consignment, nil
+}
+
+// Service should implement all of the methods to satisfy the Service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo repository
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	// Save our consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return macthing the `Response` message we created in our
+	// protobuf definition/
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main() {
+	repo := &Repository{}
+
+	// Set-up our gRPC server.
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fataf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+
+	// Register our service with the gRPC server, this will tie our
+	// implementation into the auto generated interface code for our
+	// protobuf definition.
+	pb.RegisterShippingServiceServer(s, &service{repo})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+
+	log.Println("Running on port:", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
